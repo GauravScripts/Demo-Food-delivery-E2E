188f034d4023048c94276bd59d7ea76902837a31
Gaurav <alwar.gaurav1@gmail.com>
2024-12-26 23:10:32 +0530

    Shift to postgres

--- resturantListing/src/main/java/com/udemy/resturantlisting/controller/RestaurantController.java
+++ resturantListing/src/main/java/com/udemy/resturantlisting/controller/RestaurantController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/resturant")
+@CrossOrigin(origins = "*")
 public class RestaurantController {
     @Autowired
     private ResturantService resturantService;
