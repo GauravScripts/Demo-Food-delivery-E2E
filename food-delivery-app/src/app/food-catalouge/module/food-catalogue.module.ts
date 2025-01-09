c1941bf5bba208f01c85f47c76af93a4fa72524f
Gaurav <alwar.gaurav1@gmail.com>
2025-01-10 01:44:21 +0530

    fod catalogue added

---
+++ food-delivery-app/src/app/food-catalouge/module/food-catalogue.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FoodCatalougeRoutingModule } from './food-catalouge-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FoodCatalougeRoutingModule
+  ]
+})
+export class FoodCatalogueModule { }
